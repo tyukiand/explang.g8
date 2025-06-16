1129886ea14a5acf0ac9710d00e44818cc849316
Andrey Tyukin <tyukiand@protonmail.ch>
2025-06-16 02:44:50 +0200

    [init] Add renaming master -> main

--- src/main/g8/$project-name$/git-init.sh
+++ src/main/g8/$project-name$/git-init.sh
@@ -6,6 +6,8 @@ git add project/build.properties
 git add ./src/**/*.scala
 git add .github/**/*.yml
 git add .gitignore
+git branch -m master main
+git commit -a -m '[init] Initial commit'
 
 echo "[INFO] You can now remove ./git-init.sh"
 
